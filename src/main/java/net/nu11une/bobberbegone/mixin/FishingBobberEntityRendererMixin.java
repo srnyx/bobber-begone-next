7b0a9574782fa876eedef5ef24b337277ed7c434
srnyx <25808801+srnyx@users.noreply.github.com>
2025-06-19 16:26:09 -0400

    Update to 1.21.2-1.21.6

--- src/main/java/net/nu11une/bobberbegone/mixin/FishingBobberEntityRendererMixin.java
+++ src/main/java/net/nu11une/bobberbegone/mixin/FishingBobberEntityRendererMixin.java
@@ -1,22 +1,21 @@
 package net.nu11une.bobberbegone.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.Frustum;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(net.minecraft.client.render.entity.FishingBobberEntityRenderer.class)
 public class FishingBobberEntityRendererMixin {
-	@Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/projectile/FishingBobberEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", cancellable = true)
-	public void renderCallback(FishingBobberEntity fishingBobberEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+	@Inject(at = @At("HEAD"), method = "shouldRender(Lnet/minecraft/entity/projectile/FishingBobberEntity;Lnet/minecraft/client/render/Frustum;DDD)Z", cancellable = true)
+	public void renderCallback(FishingBobberEntity fishingBobberEntity, Frustum frustum, double d, double e, double f, CallbackInfoReturnable<Boolean> cir) {
 		PlayerEntity player = MinecraftClient.getInstance().player;
 		if ((MinecraftClient.getInstance().options == null || MinecraftClient.getInstance().options.getPerspective().isFirstPerson()) && fishingBobberEntity.getHookedEntity() == player) {
-			ci.cancel();
+			cir.setReturnValue(false);
 		}
 	}
 }
